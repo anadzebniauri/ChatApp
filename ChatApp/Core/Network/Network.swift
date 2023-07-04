5fc63f454021791bb0251897d4dbac108b53be2c
anadzebniauri <anadzebniauri13@gmail.com>
2023-07-04 16:39:11 +0400

    fixed network bug

--- ChatApp/Core/Network/Network.swift
+++ ChatApp/Core/Network/Network.swift
@@ -4,54 +4,37 @@
 //
 //  Created by Ana Dzebniauri on 27.05.23.
 //
-/**
- https://www.youtube.com/watch?v=WjJBRhPiM_I
- */
 
-import Foundation
 import Network
+import SystemConfiguration
 
-final class Network {
+public class Network {
     
-    static let shared = Network()
-    
-    private let globalQueue = DispatchQueue.global()
-    private let monitor: NWPathMonitor
-    
-    public private(set) var isConnected: Bool = false
-    public private(set) var connectionType: ConnectionType = .none
-    
-    enum ConnectionType {
-        case wifi
-        case cellular
-        case none
-    }
-    
-    private init() {
-        monitor = NWPathMonitor()
-    }
-    
-    func startMonitoring() {
-        monitor.start(queue: globalQueue)
-        monitor.pathUpdateHandler = { [weak self] path in
-            self?.isConnected = path.status != .unsatisfied
-            self?.getConnectionType(path)
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in(
+            sin_len: 0,
+            sin_family: 0, sin_port: 0,
+            sin_addr: in_addr(s_addr: 0),
+            sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
         }
-    }
-    
-    func stopMonitoring() {
-        monitor.cancel()
-    }
-    
-    private func getConnectionType(_ path: NWPath) {
-        if path.usesInterfaceType(.wifi) {
-            connectionType = .wifi
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
         }
-        else if path.usesInterfaceType(.cellular) {
-            connectionType = .cellular
-        }
-        else {
-            connectionType = .none
-        }
+        
+        // Working for Cellular and WIFI
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
+        
+        return ret
     }
 }
